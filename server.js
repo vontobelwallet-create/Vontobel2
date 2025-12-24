daa8525efb793771b13bcd9ff5092135614ae078
vontobelwallet-create <vontobelwallet@gmail.com>
2025-12-24 11:52:15 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html")
 })
 
+
 app.post("/create-order", async (req, res) => {
   const orderId = "ORDER_" + Date.now();
 
@@ -28,15 +29,19 @@ app.post("/create-order", async (req, res) => {
         "x-client-id": CF_APP_ID,
         "x-client-secret": CF_SECRET
       },
-      body: JSON.stringify({
-        order_id: orderId,
-        order_amount: 10,
-        order_currency: "INR",
-        customer_details: {
-          customer_id: "cust_001",
-          customer_phone: "9999999999"
-        }
-      })
+body: JSON.stringify({
+  order_id: orderId,
+  order_amount: 10,
+  order_currency: "INR",
+  customer_details: {
+    customer_id: "cust_001",
+    customer_phone: "9999999999"
+  },
+  order_meta: {
+    return_url: "https://vontobel2.onrender.com/payment-result?order_id={order_id}"
+  }
+})
+
     });
 
     const data = await response.json();
@@ -46,6 +51,47 @@ app.post("/create-order", async (req, res) => {
     res.status(500).json({ error: "Order creation failed" });
   }
 });
+
+
+app.get("/payment-result", async (req, res) => {
+  const orderId = req.query.order_id;
+
+  res.send(`
+    <html>
+      <body style="font-family:Arial;text-align:center;padding:40px">
+        <h2>Processing Payment...</h2>
+        <script>
+          window.location.href = "https://vontobelwallet-create.github.io/Vontobel_Ledge/dashboard.html?order_id=${orderId}";
+        </script>
+      </body>
+    </html>
+  `);
+});
+
+
+app.get("/verify-payment/:orderId", async (req, res) => {
+  const orderId = req.params.orderId;
+
+  try {
+    const response = await fetch(
+      `https://api.cashfree.com/pg/orders/${orderId}`,
+      {
+        method: "GET",
+        headers: {
+          "x-api-version": "2023-08-01",
+          "x-client-id": CF_APP_ID,
+          "x-client-secret": CF_SECRET
+        }
+      }
+    );
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Verification failed" });
+  }
+});
+
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
